adf4a3ddd553311920f9165c93971c8898337fb4
Lukas <lukas@kjsoft.com.tw>
2018-08-06 18:51:56 +0800

    初步建置完成

--- POBwebbank/POBwebbank/Global.asax.cs
+++ POBwebbank/POBwebbank/Global.asax.cs
@@ -8,7 +8,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace POBwebbank {
-    public class MvcApplication : AbpWebApplication<> {
+    public class MvcApplication : AbpWebApplication<POBwebbankWebModule> {
         protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
